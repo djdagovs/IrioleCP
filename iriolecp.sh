42bd89e40c07975c1105e5290c546427ef14a1d5
Richard Hedges <richard@iriole.com>
2015-06-08 12:35:00 +0100

    Bash script improvements

--- iriolecp.sh
+++ iriolecp.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 # Iriole CP Setup
 
+function jumpto {
+	label=$1
+	cmd=$(sed -n "/$label:/{:a,;n;p;ba};" $0 | grep -v ':$')
+	eval "$cmd"
+	exit
+}
+
+start=${1:-"start"}
+
+jumpto $start
+
+start:
 echo "Welcome to the IrioleCP setup script"
-
-echo "Would you like to install IrioleCP?"
-select yn in "Yes" "No"; do
-	case $yn in
-		Yes )
-
-			echo "Beginning installation ..."
-
-		break;;
-		No ) exit;;
-	esac
-done
+read -p "Would you like to install IrioleCP? (y/n)" CONT
+echo
+if [ "$CONT" == "y" ];
+then
+	jumpto startinstall
+else
+	echo "Exiting installation..."
+	exit
+fi
---
+++ iriolecp/cp/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1>Welcome to the placeholder for the IrioleCP control panel (even though that doesn't make sense)</h1>";
+
+?>
