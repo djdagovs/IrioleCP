3089f309f31480ce3a641b4449698227cfd8a9a5
Richard Hedges <richard@iriole.com>
2015-06-27 20:34:44 +0100

    SH Installation Script Update
    Added service installations (apache, mysql-server, php, php-mysql, bind,
    bind-chroot)

--- iriolecp.sh
+++ iriolecp.sh
@@ -10,6 +10,14 @@ function jumpto
 }
 
 start=${1:-"start"}
+startinstall=${1:-"startinstall"}
+installapache=${1:-"installapache"}
+installmysql=${1:-"installmysql"}
+configuremysql=${1:-"configuremysql"}
+installphp=${1:-"installphp"}
+installingbinddns=${1:-"installingbinddns"}
+configurebinddns=${1:-"configurebinddns"}
+startiriolecp=${1:-"startiriolecp"}
 
 jumpto $start
 
@@ -23,4 +31,65 @@ then
 else
 	echo "Exiting installation..."
 	exit
+fi
+
+startinstall:
+echo "Updating packages ..."
+pause 3
+yum -y update
+jumpto $installapache
+
+installapache:
+echo "Installing Apache ..."
+pause 3
+yum -y install httpd
+jumpto $installmysql
+
+installmysql:
+echo "Installing MySQL ..."
+pause 3
+yum -y install mysql-server
+jumpto $configuremysql
+
+configuremysql:
+echo "Now to configure MySQL - you'll need to do a bit of input for this step!"
+pause 3
+service mysqld start
+/usr/bin/mysql_secure_installation
+service mysqld stop
+jumpto $installphp
+echo "Installing PHP ..."
+pause 3
+yum -y nistall php php-mysql
+goto $installingbinddns
+
+installingbinddns:
+echo "Installing DNS ..."
+pause 3
+yum -y install bind bind-utils
+goto $configurebinddns
+
+configurebinddns:
+echo "Auto conigure of BIND DNS not written yet."
+pause 1
+echo "Visit https://goo.gl/YVaJ5r for more information on how to configure BIND DNS"
+pause 3
+echo "Setting up the BIND chroot environment ..."
+pause 3
+yum -y install bind-chroot
+service named restart
+goto $startiriolecp
+
+startiriolecp:
+if (( $(ps -ef | grep -v grep | grep httpd | wc -1) > 0 ))
+then
+	echo "Apache service already running ..."
+else
+	service httpd start
+fi
+if (( $(ps -ef | grep -v grep | grep mysqld | wc -1) > 0 ))
+then
+	echo "MySQL service already running ..."
+else
+	service mysqld start
 fi
--- iriolecp/cp/index.php
+++ iriolecp/cp/index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "<h1>Welcome to the placeholder for the IrioleCP control panel (even though that doesn't make sense)</h1>";
+echo '<h1>Welcome to the placeholder for the IrioleCP control panel (even though that\'s a tautology)</h1>';
 
 ?>
